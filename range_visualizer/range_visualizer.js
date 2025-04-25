d53dfa334ece6955a8c906537394861b09db27aa
Brennan Cain <brennan@nodarsensor.com>
2025-04-25 14:40:58 -0400

    Fixed formattings, improved first detection and depth error calculations

--- range_visualizer/range_visualizer.js
+++ range_visualizer/range_visualizer.js
@@ -7,8 +7,8 @@ const CAMERA_WIDTH = 10;
 const CAMERA_HEIGHT = 10;
 const CAMERA_CENTER = WIDTH/2;
 const FOV_SIZE = HEIGHT-CAMERA_HEIGHT;
-const DISPARITY_RESOLUTION = .1;
-const DISPLAY_SCALE_TO_FIRST_OVERLAP = 100;
+const DISPLAY_SCALE_TO_FIRST_OVERLAP = 75;
+const MAX_DISPARITY = 1024;
 
 let pixelPerMeter = 100;
 
@@ -37,17 +37,29 @@ class StereoCamera {
         triangle(leftCamCenterX, CAMERA_HEIGHT, leftCamCenterX-fovWidth, FOV_SIZE+CAMERA_HEIGHT, leftCamCenterX+fovWidth, FOV_SIZE+CAMERA_HEIGHT);
         triangle(rightCamCenterX, CAMERA_HEIGHT, rightCamCenterX-fovWidth, FOV_SIZE+CAMERA_HEIGHT, rightCamCenterX+fovWidth, FOV_SIZE+CAMERA_HEIGHT);
 
-        stroke(0);
-        line(leftCamCenterX, CAMERA_HEIGHT, rightCamCenterX, CAMERA_HEIGHT);
-        text(this.baseline + " m", WIDTH/2-10, CAMERA_HEIGHT-1);
-    
         let firstOverlap = this.calculateFirstOverlap();
         line(0, firstOverlap*pixelPerMeter + CAMERA_HEIGHT, WIDTH, firstOverlap*pixelPerMeter + CAMERA_HEIGHT);
+        fill(0);
+        stroke(0);
         text(`First detection at ${firstOverlap.toPrecision(2)}m`, 10, firstOverlap*pixelPerMeter+25);
     }
 
+    calculateImagePixelDimension() {
+        // Base dimensions 2880 x 1860 => 5.4e6 pixels
+        let scaleFactor = this.imageSize/5.4e6;
+        return [
+            Math.floor(2880 * scaleFactor),
+            Math.floor(1860 * scaleFactor)
+        ];
+    }
+
+    calculateFocalLengthPixels() {
+        let imagePixelDimensions = this.calculateImagePixelDimension();
+        return (imagePixelDimensions[0] / 2) / Math.tan(radians(this.fov/2));
+    }
+
     calculateFirstOverlap() {
-        return (this.baseline/2)/Math.tan(radians(this.fov/2));
+        return this.calculateFocalLengthPixels() * this.baseline / MAX_DISPARITY;
     }
 }
 
@@ -65,12 +77,21 @@ class Sprite {
         let hits = this.calculateHits();
         let lidarHits = this.calculateLidarHits();
         image(this.image, WIDTH/2-((this.height/2-this.xOffset)*pixelPerMeter), (this.range*pixelPerMeter)+CAMERA_HEIGHT, this.height*pixelPerMeter, this.height*pixelPerMeter);
-        text(`${this.name}\n` +
-            `Range: ${this.range.toFixed(2)}m\n` +
-            `Cross-sectional Area: ${this.area.toPrecision(2)}m^2\n` +
-            `NODAR Hammerhead returns: ${hits.toFixed(0)}\n` +
-            `Velodyne HDL-64E returns: ${lidarHits.toFixed(0)}`
-            , WIDTH/2-((-this.height/2-this.xOffset)*pixelPerMeter)+30,((this.range+this.height/2))*pixelPerMeter);
+        fill(0);
+        stroke(0);
+        let textX = WIDTH/2-(this.xOffset*pixelPerMeter)+30;
+        if (this.xOffset <= 0) {
+            textX = WIDTH/2+(this.xOffset*pixelPerMeter)-205;
+        }
+        text(
+            `${this.name}\n` +
+                `Range: ${this.range.toFixed(2)}m\n` +
+                `Cross-sectional Area: ${this.area.toPrecision(2)}m²\n` +
+                `NODAR Hammerhead returns: ${hits.toFixed(0)}\n` +
+                `Velodyne HDL-64E returns: ${lidarHits.toFixed(0)}`,
+            textX,
+            ((this.range+this.height/2))*pixelPerMeter
+        );
     }
     
     calculateHits() {
@@ -92,21 +113,23 @@ let fovLabel;
 let fovSlider;
 let baselineLabel;
 let baselineSlider;
+let imageSizeLabel;
+let imageSizeSlider;
 let rangeErrorBoundLabel;
-let camera = new StereoCamera(1, 65, 5.4e6);
+let camera = new StereoCamera(1, 65, 5.4e6, 3e-6);
 let boxSprite;
 let personSprite;
-let sharkSprite;
+let sedanSprite;
 let boxRangeSlider;
 let boxRangeLabel;
 let personRangeSlider;
 let personRangeLabel;
-let sharkRangeSlider;
-let sharkRangeLabel;
+let sedanRangeSlider;
+let sedanRangeLabel;
 
 function createFOVSelector(baseY) {
     fovSlider = createSlider(15, 135, camera.fov, 1);
-    fovLabel = createP("FOV: " + camera.fov);
+    fovLabel = createP("FOV: " + camera.fov + "°");
     fovSlider.position(10, baseY);
     fovLabel.position(150, baseY-15);
 
@@ -118,7 +141,7 @@ function createFOVSelector(baseY) {
 
 function createBaselineSelector(baseY) {
     baselineSlider = createSlider(0.1, 3, camera.baseline, 0.1);
-    baselineLabel = createP("Baseline: " + camera.baseline);
+    baselineLabel = createP("Baseline: " + camera.baseline + "m");
     baselineSlider.position(10, baseY);
     baselineLabel.position(150, baseY-15);
 
@@ -141,7 +164,7 @@ function createImageSizeSelector(baseY) {
 }
 
 function createBoxRangeSelector(baseX, baseY) {
-    boxRangeSlider = createSlider(.1, 25, boxSprite.range, .1);
+    boxRangeSlider = createSlider(.1, 25, Math.sqrt(boxSprite.range), .1);
     boxRangeLabel = createP("Box Range: " + boxSprite.range + "m");
     boxRangeSlider.position(baseX, baseY);
     boxRangeLabel.position(baseX + 140, baseY-15);
@@ -153,7 +176,7 @@ function createBoxRangeSelector(baseX, baseY) {
 }
 
 function createPersonRangeSelector(baseX, baseY) {
-    personRangeSlider = createSlider(.1, 25, personSprite.range, .1);
+    personRangeSlider = createSlider(.1, 25, Math.sqrt(personSprite.range), .1);
     personRangeLabel = createP("Person Range: " + personSprite.range + "m");
     personRangeSlider.position(baseX, baseY);
     personRangeLabel.position(baseX + 140, baseY-15);
@@ -164,15 +187,15 @@ function createPersonRangeSelector(baseX, baseY) {
     });
 }
 
-function createSharkRangeSelector(baseX, baseY) {
-    sharkRangeSlider = createSlider(.1, 25, sharkSprite.range, .1);
-    sharkRangeLabel = createP("Shark Range: " + sharkSprite.range + "m");
-    sharkRangeSlider.position(baseX, baseY);
-    sharkRangeLabel.position(baseX + 140, baseY-15);
+function createSedanRangeSelector(baseX, baseY) {
+    sedanRangeSlider = createSlider(.1, 25, Math.sqrt(sedanSprite.range), .1);
+    sedanRangeLabel = createP("Sedan Range: " + sedanSprite.range + "m");
+    sedanRangeSlider.position(baseX, baseY);
+    sedanRangeLabel.position(baseX + 140, baseY-15);
 
-    sharkRangeSlider.input(function() {
-        sharkSprite.range = Math.pow(sharkRangeSlider.value() ,2);
-        sharkRangeLabel.html("Shark Range: " + sharkSprite.range.toFixed(2) + "m");
+    sedanRangeSlider.input(function() {
+        sedanSprite.range = Math.pow(sedanRangeSlider.value() ,2);
+        sedanRangeLabel.html("Sedan Range: " + sedanSprite.range.toFixed(2) + "m");
     });
 }
 
@@ -184,22 +207,32 @@ function createRangeErrorBoundLabel(baseX, baseY) {
 }
 
 function updateRangeBounds() {
-    rangeErrorBoundLabel.html("Error Bounds:<br>" +
-        "5m: " + calculateRangeErrorBound(5).toPrecision(2) + " m<br>" +
-        "10m: " + calculateRangeErrorBound(10).toPrecision(2) + " m<br>" +
-        "20m: " + calculateRangeErrorBound(20).toFixed(2) + " m<br>" +
-        "50m: " + calculateRangeErrorBound(50).toFixed(2) + " m<br>" +
-        "100m: " + calculateRangeErrorBound(100).toFixed(2) + " m<br>" +
-        "200m: " + calculateRangeErrorBound(200).toFixed(2) + " m<br>" +
-        "500m: " + calculateRangeErrorBound(500).toFixed(2) + " m<br>");
+    rangeErrorBoundLabel.html("Depth Precision:<br>" +
+        "5m:   ±" + calculateRangeErrorBound(5).toPrecision(2) + " m<br>" +
+        "10m:  ±" + calculateRangeErrorBound(10).toPrecision(2) + " m<br>" +
+        "20m:  ±" + calculateRangeErrorBound(20).toFixed(2) + " m<br>" +
+        "50m:  ±" + calculateRangeErrorBound(50).toFixed(2) + " m<br>" +
+        "100m: ±" + calculateRangeErrorBound(100).toFixed(2) + " m<br>" +
+        "200m: ±" + calculateRangeErrorBound(200).toFixed(2) + " m<br>" +
+        "500m: ±" + calculateRangeErrorBound(500).toFixed(2) + " m<br>");
 }
 
 
 function calculateRangeErrorBound(range) {
+    // resolution = 0.1 if disparity <128 , 0.2 if disparity < 512 and 0.4 if disparity < 1024
+    let disparity = camera.calculateFocalLengthPixels() * camera.baseline / range;
+    let disparityResolution = 0.1;
+    if (disparity > 128) {
+        disparityResolution = 0.2;
+    }
+    if (disparity > 512) {
+        disparityResolution = 0.4;
+    }
+
     let a = camera.fov/2;
     let A = sqrt(camera.imageSize)/2;
     let focalLengthPixels = A/tan(radians(a));
-    return range*range*DISPARITY_RESOLUTION/(focalLengthPixels* camera.baseline);
+    return range*range*disparityResolution/(focalLengthPixels* camera.baseline);
 }
 
 // Places the first overlap line at 1/$(DISPLAY_SCALE_TO_FIRST_OVERLAP)th of the full image
@@ -211,16 +244,16 @@ function updateScale() {
 function setup() {
     createCanvas(WIDTH, HEIGHT);
 
-    boxSprite = new Sprite(loadImage('box.png'), 1, 0, .5, .01, "10cm Box");
-    personSprite =  new Sprite(loadImage('person.png'), 5, 1, 2, .75, "Person");
-    sharkSprite = new Sprite(loadImage('shark.png'), 15,-1, 5, 1.5, "Hammerhead Shark");
+    boxSprite = new Sprite(loadImage('box.png'), 5, 0, .5, .01, "Box (10cm x 10cm)");
+    personSprite =  new Sprite(loadImage('person.png'), 15, 1, 2, .75, "Person");
+    sedanSprite = new Sprite(loadImage('sedan-rear.png'), 30,-1, 1.75, 2.5, "Sedan");
 
     createFOVSelector(140);
     createBaselineSelector(170);
     createImageSizeSelector(200);
     createBoxRangeSelector(400, 140);
     createPersonRangeSelector(400, 170);
-    createSharkRangeSelector(400, 200);
+    createSedanRangeSelector(400, 200);
     createRangeErrorBoundLabel(700, 95);
 
 }
@@ -232,5 +265,5 @@ function draw() {
     updateRangeBounds();
     boxSprite.draw();
     personSprite.draw();
-    sharkSprite.draw();
+    sedanSprite.draw();
 }
